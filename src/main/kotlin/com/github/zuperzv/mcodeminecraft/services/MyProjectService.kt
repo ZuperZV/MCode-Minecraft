c9e93bab0dd05469b7b546e752391c7be06f7bde
GitHub Action <action@github.com>
2026-02-05 20:42:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/zuperzv/mcodeminecraft/services/MyProjectService.kt
+++ src/main/kotlin/com/github/zuperzv/mcodeminecraft/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.zuperzv.mcodeminecraft.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zuperzv.mcodeminecraft.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
